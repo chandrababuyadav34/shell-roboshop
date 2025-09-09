f541656b296e4a23bbcb01769fd5f1909488cee7
chandrababuyadav34 <splendidchandu34@gmail.com>
2025-09-09 16:50:34 +0530

    cvn

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ ZONE_ID="Z01733572HPL17T8X7GR9" # replace with your ZONE ID
 DOMAIN_NAME="chandu.fun" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in "$@"
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0fd59b8e9516cdcfd --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
