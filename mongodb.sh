ca672297db32670d970226cf1d19b500afab3756
chandrababuyadav34 <splendidchandu34@gmail.com>
2025-09-10 17:41:08 +0530

    mongodb instances

--- mongodb.sh
+++ mongodb.sh
@@ -22,7 +22,7 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
+# validate functions takes input as exit status, what command they tried to instal
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
